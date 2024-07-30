280a604174da085348a2fce9a45374e428daab85
bregarulai <bregarulai@gmail.com>
2024-07-30 18:48:31 -0400

    implement document list in Home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,12 +5,18 @@ import Image from "next/image";
 import AddDocumentButton from "@/components/AddDocumentButton";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { getDocuments } from "@/lib/actions/room.actions";
+import Link from "next/link";
+import { dateConverter } from "@/lib/utils";
 
 const Home = async () => {
   const clerkUser = await currentUser();
   if (!clerkUser) redirect("/sign-in");
 
-  const documents = [];
+  const roomDocuments = await getDocuments(
+    clerkUser.emailAddresses[0].emailAddress
+  );
+  console.log("Room Documents: ", roomDocuments);
   return (
     <div className="home-container">
       <Header className="sticky left-0 top-0">
@@ -21,8 +27,44 @@ const Home = async () => {
           </SignedIn>
         </div>
       </Header>
-      {documents.length > 0 ? (
-        <div></div>
+      {roomDocuments.data.length > 0 ? (
+        <div className="document-list-container">
+          <div className="document-list-title">
+            <h3 className="text-28-semibold">All documents</h3>
+            <AddDocumentButton
+              userId={clerkUser.id}
+              email={clerkUser.emailAddresses[0].emailAddress}
+            />
+          </div>
+          <ul className="document-ul">
+            {roomDocuments.data.map(
+              ({ id, metadata: { title }, createdAt }: any) => (
+                <li key={id} className="document-list-item">
+                  <Link
+                    href={`/documents/${id}`}
+                    className="flex flex-1 items-center gap-4"
+                  >
+                    <div className="hidden rounded-md bg-dark-500 p-2 sm:block">
+                      <Image
+                        src={"/assets/icons/doc.svg"}
+                        height={40}
+                        width={40}
+                        alt="file"
+                      />
+                    </div>
+                    <div className="space-y-1">
+                      <p className="line-clamp-1 text-lg">{title}</p>
+                      <p className="text-sm font-light text-blue-100">
+                        Created about {dateConverter(createdAt)}
+                      </p>
+                    </div>
+                  </Link>
+                  {/*TODO: Delete Button */}
+                </li>
+              )
+            )}
+          </ul>
+        </div>
       ) : (
         <div className="document-list-empty">
           <Image
--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -76,3 +76,13 @@ export const updateDocument = async (roomId: string, title: string) => {
     console.error(`Error happened while updating a room: ${error}`);
   }
 };
+
+export const getDocuments = async (email: string) => {
+  try {
+    const rooms = await liveblocks.getRooms({ userId: email });
+
+    return parseStringify(rooms);
+  } catch (error) {
+    console.error(`Error happened while getting rooms: ${error}`);
+  }
+};
