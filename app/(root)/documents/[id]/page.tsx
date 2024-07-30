4b1200ab6ac978d9d3723ce0494bff7c0d1fada2
bregarulai <bregarulai@gmail.com>
2024-07-30 12:09:51 -0400

    implement adding CollaborativeRoom component to Document page

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,10 +1,11 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-
-import { Editor } from "@/components/editor/Editor";
-import Header from "@/components/Header";
+import CollaborativeRoom from "@/components/CollaborativeRoom";
 
 const Document = () => {
-  return <div></div>;
+  return (
+    <main className="flex w-full flex-col items-center">
+      <CollaborativeRoom />
+    </main>
+  );
 };
 
 export default Document;
