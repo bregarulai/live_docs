e0676a8824e3310a2b02a6257d3023c5c7fd3ab8
bregarulai <bregarulai@gmail.com>
2024-07-30 13:11:01 -0400

    impliment getDocument action, roomId and roomMetada props to CollaborativeRoom component

--- types/index.d.ts
+++ types/index.d.ts
@@ -12,3 +12,32 @@ declare type AddDocumentButtonProps = {
   userId: string;
   email: string;
 };
+
+declare type SearchParamProps = {
+  params: { [key: string]: string };
+  searchPrams: { [key: string]: string | string[] | undefined };
+};
+
+declare type RoomMetadata = {
+  creatorId: string;
+  email: string;
+  title: string;
+};
+
+declare type User = {
+  id: string;
+  name: string;
+  email: string;
+  avatar: string;
+  color: string;
+  userType?: UserType;
+};
+
+declare type UserType = "creator" | "editor" | "viewer";
+
+declare type CollaborativeRoomProps = {
+  roomId: string;
+  roomMetadata: RoomMetadata;
+  users?: User[];
+  currentUserType?: UserType;
+};
